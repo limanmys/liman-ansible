d02d04ab4c44440f7446967596027e8868b19696
mustafaakbel <mustafaakbel@outlook.com>
2020-12-17 14:46:04 +0300

    liman 1.5 update function.php

--- app/App.php
+++ app/App.php
@@ -7,16 +7,6 @@ class App
 {
 	public function init()
 	{
-		$routes = include getPath('routes.php');
-
-		foreach ($routes as $route => $destination) {
-			Route::add($route, function () use ($destination) {
-				$destination = explode('@', $destination);
-				$class = 'App\\Controllers\\' . $destination[0];
-				return (new $class())->{$destination[1]}();
-			});
-		}
-
-		eval(Route::make());
+		//will be called when extension init
 	}
 }
--- views/functions.php
+++ views/functions.php
@@ -1,5 +1,16 @@
 <?php
 require_once getPath('vendor/autoload.php');
 
-use App\App;
-(new App())->init();
+use App\Helpers\Route;
+
+$routes = include getPath('routes.php');
+
+foreach ($routes as $route => $destination) {
+	Route::add($route, function () use ($destination) {
+		$destination = explode('@', $destination);
+		$class = 'App\\Controllers\\' . $destination[0];
+		return (new $class())->{$destination[1]}();
+	});
+}
+
+eval(Route::make());
