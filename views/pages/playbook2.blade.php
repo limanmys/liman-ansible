d0e0bcf543833254ea7f1919eebfe08ece1aac62
susalihh <susalihh@gmail.com>
2021-12-30 14:02:34 +0300

    some frontend changes

--- views/pages/playbook2.blade.php
+++ views/pages/playbook2.blade.php
@@ -1,27 +1,29 @@
-<div class="container-fluid mt-5">
-    <div class="row">
-      <div class="col-sm-2">
-        <button class="btn btn-primary mb-2" onclick="runPlaybook2()" type="button" 
-        style="width:100%;">
+<div class="row">
+    <div>
+        <button style="width:100%;" class="btn btn-primary mb-2" onclick="runPlaybook2()" type="button">
             <i class="far fa-play-circle mr-2"></i> {{ __('Çalıştır') }}
-        </button> <br>
-        
-        <select id="dropdown1" style="width:100%;"></select>
-            <div class="mb-1"></div>
-        <input type="password" name="sudoPassword" id="sudopass_field" class="container-sm"
-            placeholder="Sudo şifresini giriniz" style="width:100%;">
-            <br><br>
-            <p id="test"></p>
-      </div>
-      <div class="col-sm-5">
+        </button> 
+    </div>
+    <div class="col-sm-2">
+        <select style="width:100%;" id="dropdown1" class="select2 select2-container select2-container--bootstrap4 select2-container--below select2-container--focus"></select>
+    </div>
+    <div class="col-sm-2">
+        <input style="width:100%;" type="password" name="sudoPassword" id="sudopass_field" class="form-control"
+        placeholder="Sudo şifresini giriniz">
+    </div>
+  </div>
+
+<div class="container-fluid mt-3">
+    <div class="row">
+      <div class="col-sm-6">
           <div class="col" id="fileTextDiv2">
-            <textarea style="width:100%;height: 80%; min-height: 400px;" id="textDiv2"></textarea>
+            <textarea style="width:100%;height: 100%;min-height:550px;" id="textDiv2"></textarea>
             <button  class="btn btn-primary mb-2 float-right" id="fileEditButton" onclick="saveLogPlaybook2()">
                 <i class="fas fa-edit" ></i> {{ __('Kaydet') }}
             </button>
         </div>
       </div>
-      <div class="col-sm-5">
+      <div class="col-sm-6">
         <div id="logTable1"></div>
             @component('modal-component',[
             "id" => "showLogContentComponent2",
@@ -33,11 +35,31 @@
   </div>
 
 <script>
-    getLog2();
-    function getLog2(){
+    function getPlaybooks2(){
+        $('#textDiv2').html('');
+        $('#sudopass_field').val('');
         let form = new FormData();
         showSwal('{{__("Yükleniyor...")}}','info');
-        request(API('get_log2'), form, function(response) {
+        request(API('get_playbooks2'), form, function(response) {
+
+        const myArray = response.split("\n");
+        document.getElementById("dropdown1").options.length=0;
+        for(let i=0;i<myArray.length-1;i++){
+            var opt = document.createElement("option");
+            opt.text = myArray[i];
+            opt.value = myArray[i];
+            
+            document.getElementById("dropdown1").options.add(opt);
+        }
+        Swal.close();
+        }, function(error) {
+            error = JSON.parse(error)["message"]
+            showSwal(error,'error');
+        });
+            //////////table///////////
+            let data = new FormData();
+        showSwal('{{__("Yükleniyor...")}}','info');
+        request(API('get_log2'), data, function(response) {
             $('#logTable1').html(response).find('table').DataTable(dataTablePresets('normal'));
             Swal.close();
         }, function(error) {
@@ -45,6 +67,7 @@
             showSwal(error,'error');
         });
     }
+    
     function showLogContent2(line){
         let fileName = line.querySelector("#name").innerHTML + "-.-" + line.querySelector("#user").innerHTML;
         let formData = new FormData()
@@ -76,33 +99,13 @@
                 let formData = new FormData();
                 formData.append("fileName",fileName);
                 request(API("delete_log2") ,formData,function(response){
+                    getPlaybooks2();
                     showSwal('{{__("Silindi")}}', 'success',2000);
                 }, function(response){
                     let error = JSON.parse(response);
                     showSwal(error.message, 'error');
                 });
             }
-        });
-    }
-
-    function getPlaybooks2(){
-        let form = new FormData();
-        showSwal('{{__("Yükleniyor...")}}','info');
-        request(API('get_playbooks2'), form, function(response) {
-
-        const myArray = response.split("\n");
-        document.getElementById("dropdown1").options.length=0;
-        for(let i=0;i<myArray.length-1;i++){
-            var opt = document.createElement("option");
-            opt.text = myArray[i];
-            opt.value = myArray[i];
-            
-            document.getElementById("dropdown1").options.add(opt);
-        }
-        Swal.close();
-        }, function(error) {
-            error = JSON.parse(error)["message"]
-            showSwal(error,'error');
         });
     }
 
@@ -145,8 +148,8 @@
                 let formData = new FormData();
                 formData.append("logFileName", result.value);
                 request(API("playbook2_save_output") ,formData,function(response){
+                    getPlaybooks2();
                     $('#playbookTaskModal').modal('hide');
-                    getLog2();
                     showSwal('{{__("Kaydedildi")}}', 'success',2000);
                 }, function(response){
                     let error = JSON.parse(response);
